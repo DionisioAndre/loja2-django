da0259fcee8726afb712a5afe2ca0ec21dfc0994
DionisioAndre <92489545+DionisioAndre@users.noreply.github.com>
2024-10-31 16:52:33 +0000

    Update create_superuser.py

--- create_superuser.py
+++ create_superuser.py
@@ -12,9 +12,14 @@ username = 'admin'
 email = 'admin@gmail.com'
 password = '1234'
 
-# Criação do superusuário
-if not User.objects.filter(username=username).exists():
+# Tente obter o superusuário
+try:
+    user = User.objects.get(username=username)
+    # Atualiza a senha se o usuário já existir
+    user.set_password(password)
+    user.save()
+    print("Superusuário atualizado com sucesso!")
+except User.DoesNotExist:
+    # Criação do superusuário se não existir
     User.objects.create_superuser(username=username, email=email, password=password)
     print("Superusuário criado com sucesso!")
-else:
-    print("Superusuário já existe.")
